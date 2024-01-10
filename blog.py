4b85727962dc9c16863a381c8f71f8dc4a6b5e2d
tajaa <tajatheprince@gmail.com>
2024-01-09 23:52:35 -0800

    first commit of flask blog

---
+++ blog.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Bobby Digital",
+        "title": "Blog Post 1",
+        "content": "First post conent",
+        "date_posted": "April 20, 2023",
+    },
+    {
+        "author": "Jane Digital",
+        "title": "Blog Post 2",
+        "content": "First post conent",
+        "date_posted": "February 20, 2023",
+    },
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
