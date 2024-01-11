ecf85c9de99d588818dbd7eab53a27fa17e84446
tajaa <tajatheprince@gmail.com>
2024-01-11 15:50:19 -0800

    added the register and login pages but there's no databse to hold the users yet

--- blog.py
+++ blog.py
@@ -1,23 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, url_for
 
 from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "8f2f260684695eb8fce48a35302dd139"
 
+app.config["SECRET_KEY"] = "8f2f260684695eb8fce48a35302dd139"
 posts = [
     {
-        "author": "John Foe",
+        "author": "Corey Schafer",
         "title": "Blog Post 1",
-        "content": "First post conent",
-        "date_posted": "April 20, 2023",
+        "content": "First post content",
+        "date_posted": "April 20, 2018",
     },
     {
         "author": "Jane Doe",
         "title": "Blog Post 2",
-        "content": "First post conent",
-        "date_posted": "February 20, 2023",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018",
     },
 ]
 
@@ -30,18 +30,27 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="About Us")
+    return render_template("about.html", title="About")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
     return render_template("register.html", title="Register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "thispassword":
+            flash("You have been logged in", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("login unsuccessful. Please check username and password", "danger")
     return render_template("login.html", title="Login", form=form)
 
 
