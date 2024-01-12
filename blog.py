b178934cc22aee6e7beab1b3c407fe0627ede6ef
tajaa <tajatheprince@gmail.com>
2024-01-12 00:29:36 -0800

    added sql alchemy

--- blog.py
+++ blog.py
@@ -1,11 +1,41 @@
+from datetime import datetime
+
 from flask import Flask, flash, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
 
-
 app.config["SECRET_KEY"] = "8f2f260684695eb8fce48a35302dd139"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
 posts = [
     {
         "author": "Corey Schafer",
@@ -46,11 +76,11 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "admin@blog.com" and form.password.data == "thispassword":
-            flash("You have been logged in", "success")
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
             return redirect(url_for("home"))
         else:
-            flash("login unsuccessful. Please check username and password", "danger")
+            flash("Login Unsuccessful. Please check username and password", "danger")
     return render_template("login.html", title="Login", form=form)
 
 
